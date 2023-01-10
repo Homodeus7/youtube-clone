b501f7268f139f8308091aba2a2573156d282b4b
homodeus7 <plum.bus@yahoo.com>
2023-01-10 20:36:29 +0300

    split the videoitem component

---
+++ src/components/VideoItemInfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="flex items-start mt-3">
+    <img
+      :src="`https://picsum.photos/seed/avatar${index}/68`"
+      class="mr-3 rounded-full w-9 h-9"
+      alt=""
+    />
+    <div class="text-sm">
+      <span class="font-semibold text-gray-800">Video title</span>
+      <div class="mt-1 flex items-center">
+        <span>Channel name</span>
+        <svg
+          class="w-3.5 h-3.5 ml-1"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <span>1K views</span>
+        &middot;
+        <span>1 day ago</span>
+      </div>
+    </div>
+    <button
+      class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none"
+    >
+      <svg
+        class="w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
+        ></path>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["index"],
+};
+</script>

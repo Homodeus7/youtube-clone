04324f9781ec9042841938c6559adc8b71db1ee0
homodeus7 <plum.bus@yahoo.com>
2023-01-11 17:08:17 +0300

    added sidebarContent

---
+++ src/components/SidebarContent.vue
@@ -0,0 +1,74 @@
+<template>
+  <SidebarSection>
+    <ul>
+      <SidebarNavItem label="Home" icon="home" is-active />
+      <SidebarNavItem label="Trending" icon="fire" />
+      <SidebarNavItem label="Subscriptions" icon="collection" />
+    </ul>
+  </SidebarSection>
+  <SidebarSection>
+    <ul>
+      <SidebarNavItem label="Library" icon="duplicate" />
+      <SidebarNavItem label="History" icon="clock" />
+    </ul>
+  </SidebarSection>
+  <SidebarSection class="px-8 py-4 font-medium leading-5 border-b">
+    <span class="text-sm">
+      Sign in to like videos, comment, and subscribe.
+    </span>
+    <ButtonLogin />
+  </SidebarSection>
+  <SidebarSection>
+    <SidebarSectionHeading title="Best of YouTube" />
+    <ul>
+      <SidebarNavItem label="Music" icon="musicNote" />
+      <SidebarNavItem label="Sports" icon="speakerphone" />
+      <SidebarNavItem label="Gaming" icon="puzzle" />
+      <SidebarNavItem label="Movies" icon="film" />
+      <SidebarNavItem label="News" icon="newspaper" />
+      <SidebarNavItem label="Live" icon="statusOnline" />
+      <SidebarNavItem label="360&deg; Video" icon="eye" />
+    </ul>
+  </SidebarSection>
+  <SidebarSection>
+    <ul>
+      <SidebarNavItem label="Browse channels" icon="plusCircle" />
+    </ul>
+  </SidebarSection>
+  <SidebarSection>
+    <SidebarSectionHeading title="More from YouTube" />
+    <ul>
+      <SidebarNavItem label="YouTube Premium" icon="play" />
+      <SidebarNavItem label="Live" icon="statusOnline" />
+    </ul>
+  </SidebarSection>
+  <SidebarSection>
+    <ul>
+      <SidebarNavItem label="Settings" icon="cog" />
+      <SidebarNavItem label="Report history" icon="flag" />
+      <SidebarNavItem label="Help" icon="questionMarkCircle" />
+      <SidebarNavItem label="Seend feedback" icon="annotation" />
+    </ul>
+  </SidebarSection>
+  <SidebarSection class="px-6 py-4 text-xs font-semibold space-y-4">
+    <TheFooter />
+  </SidebarSection>
+</template>
+
+<script>
+import SidebarNavItem from "./SidebarNavItem.vue";
+import SidebarSectionHeading from "./SidebarSectionHeading.vue";
+import SidebarSection from "./SidebarSection.vue";
+import ButtonLogin from "./ButtonLogin.vue";
+import TheFooter from "./TheFooter.vue";
+
+export default {
+  components: {
+    SidebarNavItem,
+    ButtonLogin,
+    SidebarSectionHeading,
+    SidebarSection,
+    TheFooter,
+  },
+};
+</script>

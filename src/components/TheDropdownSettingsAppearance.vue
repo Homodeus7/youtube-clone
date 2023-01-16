b74292a0d2c52776a2ce185ded61f27143c906d3
homodeus7 <plum.bus@yahoo.com>
2023-01-16 19:43:43 +0300

    added TheDropdownSettingsAppearance

---
+++ src/components/TheDropdownSettingsAppearance.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="py-2 border-b">
+    <ul>
+      <DropdownSettingsListItem
+        v-for="listItem in listItems.slice(0, 8)"
+        :key="listItem.label"
+        :label="listItem.label"
+      />
+    </ul>
+  </section>
+</template>
+
+<script>
+import DropdownSettingsListItem from "./DropdownSettingsListItem.vue";
+
+export default {
+  props: ["icon", "label"],
+
+  components: {
+    DropdownSettingsListItem,
+  },
+
+  data() {
+    return {
+      listItems: [
+        {
+          label: "Use devise theme",
+        },
+        {
+          label: "Dark theme",
+        },
+        {
+          label: "Light theme",
+        },
+      ],
+    };
+  },
+};
+</script>

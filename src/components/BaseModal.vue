dd658307e221c58f7e9e963d716f9d1a5f9dca58
homodeus7 <plum.bus@yahoo.com>
2023-01-23 20:00:42 +0300

    created a modal component

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fixed inset-0 z-10 bg-black bg-opacity-80">
+    <div class="bg-white max-w-sm mx-auto my-8">
+      <div class="p-2 text-right">
+        <BaseModalButtonClose />
+      </div>
+      <div class="p-6">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        placeat. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa
+        illo doloremque eaque eum asperiores aspernatur accusamus voluptatem
+        maiores obcaecati dicta! Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Ipsa illo doloremque eaque eum asperiores aspernatur
+        accusamus voluptatem maiores obcaecati dicta!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseModalButtonClose from "./BaseModalButtonClose.vue";
+
+export default {
+  components: {
+    BaseModalButtonClose,
+  },
+};
+</script>
--- src/components/TheSearchWrapper.vue
+++ src/components/TheSearchWrapper.vue
@@ -11,11 +11,13 @@
         <BaseIcon name="microphone" class="w-5 h-5" />
       </button>
     </BaseTooltip>
+    <BaseModal />
   </div>
 </template>
 
 <script>
 import BaseIcon from "./BaseIcon.vue";
+import BaseModal from "./BaseModal.vue";
 import BaseTooltip from "./BaseTooltip.vue";
 import TheSearch from "./TheSearch.vue";
 
@@ -24,6 +26,7 @@ export default {
     BaseIcon,
     BaseTooltip,
     TheSearch,
+    BaseModal,
   },
 
   props: ["isSmallScreen"],

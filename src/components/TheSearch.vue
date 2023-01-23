a0e2ba55245e654aab48a90f3310be3f322469f3
homodeus7 <plum.bus@yahoo.com>
2023-01-23 19:42:30 +0300

    bug fixing and refactoring

--- src/components/TheSearch.vue
+++ src/components/TheSearch.vue
@@ -141,9 +141,10 @@ export default {
     },
 
     selectSearchResult() {
-      this.query = this.activeSearchResultId
-        ? this.results[this.activeSearchResultId]
-        : this.query;
+      this.query =
+        this.activeSearchResultId === null
+          ? this.query
+          : this.results[this.activeSearchResultId];
 
       this.toggleSearchResults(false);
 

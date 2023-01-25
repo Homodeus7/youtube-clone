f8d0e0d4ef0d925ff7dfcb5774e3e74e37bda4be
homodeus7 <plum.bus@yahoo.com>
2023-01-25 14:54:25 +0300

    added basebutton

--- src/components/TheModalSearchPredictions.vue
+++ src/components/TheModalSearchPredictions.vue
@@ -6,51 +6,33 @@
       :search-predictions="searchPredictions"
     />
     <template #footer="{ close }">
-      <button
-        @click="close"
-        class="uppercase text-sm font-medium tracking-wider py-2.5 px-4 ml-auto mr-1 focus:ouline-none"
+      <BaseButton @click="close" class="ml-auto">Cancel</BaseButton>
+      <BaseButton class="ml-1 text-gray-400 cursor-auto" disabled
+        >Report</BaseButton
       >
-        Cancel
-      </button>
-      <button
-        class="uppercase text-sm font-medium tracking-wider py-2.5 px-4 ml-1 mr-1 focus:ouline-none text-gray-400 cursor-auto"
-        disabled
-      >
-        Report
-      </button>
     </template>
   </BaseModal>
 </template>
 
 <script>
 import BaseModal from "./BaseModal.vue";
+import BaseButton from "./BaseButton.vue";
 import TheSearchPredictionsList from "./TheSearchPredictionsList.vue";
 
 export default {
   components: {
     BaseModal,
     TheSearchPredictionsList,
+    BaseButton,
+  },
+
+  props: {
+    searchPredictions: Array,
   },
 
   data() {
     return {
       selectedSearchPredictions: [],
-      searchPredictions: [
-        "new york giants",
-        "new york alicia keys",
-        "new york giants vs washington football",
-        "new york",
-        "new york song",
-        "new york new york frank sinatra",
-        "new york jets",
-        "new york city",
-        "new york giants live",
-        "new york state of mind",
-        "new york giants vs washington football live",
-        "new york giants injury",
-        "new york giants live stream",
-        "new york accent",
-      ],
     };
   },
 };

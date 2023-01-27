9b76763db163df757d1c40a29b810a04ccb18351
homodeus7 <plum.bus@yahoo.com>
2023-01-27 14:53:36 +0300

    added radioBase component

---
+++ src/components/TheSearchPredictionCategories.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="space-y-3 text-black text-sm mt-5">
+    <div
+      v-for="(label, index) in categories"
+      :key="label"
+      class="flex items-center"
+    >
+      <BaseRadio
+        :id="`search-prediction-category-${index}`"
+        :value="label"
+        v-model="selectedSearchPredictionCategory"
+      >
+        {{ label }}
+      </BaseRadio>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseRadio from "./BaseRadio.vue";
+
+export default {
+  components: {
+    BaseRadio,
+  },
+
+  props: {
+    categories: Array,
+    modelValue: String,
+  },
+
+  emits: ["update:modelValue"],
+
+  data() {
+    return {
+      selectedSearchPredictionCategory: this.modelValue,
+    };
+  },
+
+  watch: {
+    selectedSearchPredictionCategory() {
+      this.$emit("update:modelValue", this.selectedSearchPredictionCategory);
+    },
+  },
+};
+</script>

8beacb077a6405df84c14d21437cc3d6b876ef81
homodeus7 <plum.bus@yahoo.com>
2023-01-27 14:54:18 +0300

    added baseradio component

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    type="radio"
+    class="h-5 w-5 cursor-pointer"
+    :id="id"
+    v-model="selectedSearchPredictionCategory"
+  />
+  <label v-if="$slots.default" :for="id" class="pl-4 cursor-pointer flex-grow">
+    <slot />
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    modelValue: String,
+  },
+  emits: ["update:modelValue"],
+
+  data() {
+    return {
+      selectedSearchPredictionCategory: this.modelValue,
+    };
+  },
+
+  watch: {
+    selectedSearchPredictionCategory() {
+      this.$emit("update:modelValue", this.selectedSearchPredictionCategory);
+    },
+  },
+};
+</script>
